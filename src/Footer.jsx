fb48969862339d80415cfd1092fb4ec3a10bc453
Juan Cabrera <juan@Juans-MBP.attlocal.net>
2025-04-24 12:37:23 -0500

    added privacy policies and updated sitemap

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 const Footer = () => (
   <footer className="bg-blue-700 text-white text-center py-4 mt-16">
     <p>&copy; 2025 Domi Websites | All Rights Reserved.</p>
+    <p className="mt-2">
+      <a
+        href="/privacy-policy.html"
+        className="text-white text-sm underline hover:text-gray-300"
+      >
+        Privacy Policy
+      </a>
+    </p>
   </footer>
 );
 
